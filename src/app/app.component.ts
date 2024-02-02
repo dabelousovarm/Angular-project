1fa7aeedcbe77e18d45c6652c4a1f84e334cf237
Roman Borisov <rborisov@codemastersintl.com>
2024-02-02 09:47:47 +0530

    added feedback

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  // Старайся не оставлять неиспользуемый код
   title = 'angular-project';
 }
--- src/app/components/product-info/product-info.component.ts
+++ src/app/components/product-info/product-info.component.ts
@@ -16,6 +16,11 @@ export class ProductInfoComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    /* Я уже писал про это, но немного дополню
+    * Если у тебя переменная которую ты задаешь внутри subscribe используется только в шаблоне, то можно обойтись без подписки при помощи AsyncPipe
+    * Если же переменная используется в других местах внутри компонента, то тогда, ВОЗМОЖНО, стоит использовать подписку
+    * тут ты можешь обойтись без подписки, так как переменная используется только в шаблоне (тут тебе пригодится map внутри pipe)
+    * */
     this.productSubscribtion = this.route.data.subscribe((data) => {
       this.product = data['data'];
     });
